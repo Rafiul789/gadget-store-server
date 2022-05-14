1c0c10c8f4812d2dfa2e39dc2ff3b44f9eac38b7
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-05-14 11:51:58 +0600

    gitignore file added

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ try{
 await client.connect();
 
 const serviceCollection=client.db('gadget-store').collection('item');
+// Get
 app.get('/service',async(req,res)=>{
   const query={};
 const cursor=serviceCollection.find(query);
@@ -35,12 +36,13 @@ app.get('/service/:id',async(req,res)=>{
   const service=await serviceCollection.findOne(query)
   res.send(service)
 })
-
+// Post
 app.post('/service',async(req,res)=>{
   const newService=req.body;
 const result=await serviceCollection.insertOne(newService);
 req.send(result)
 
+// Delete
 app.delete('/service/:id',async(req,res)=>{
   const id=req.params.id;
   const query={_id:ObjectId(id)};
